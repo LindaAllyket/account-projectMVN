b44e4625c100783dc0f8dc0a866a05242411fddd
LindaAllyket <Linda.Biasua@academytrainee.com>
2019-05-22 16:18:29 +0100

    repository added

--- src/main/java/main/Account.java
+++ src/main/java/main/Account.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity // to make it identifiable and manageable
+@Entity // to make it identifiable and manageable i.e. bean
 public class Account {
 
 	private String firstName;
